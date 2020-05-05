c83d394d08b99bd8999e551268121628d5a71e6a
Ignazio Secci <ignazio.secci@welld.ch>
2020-05-05 14:06:02 +0000

    feat(cop): add rules for control devices

---
+++ src/main/java/ch/welld/schindler/fixture/droolsknowledge/builders/ControlDeviceConfigurationBuilder.java
@@ -0,0 +1,62 @@
+package ch.welld.schindler.fixture.droolsknowledge.builders;
+
+import ch.welld.schindler.fixture.droolsknowledge.components.controldevices.ControlDeviceConfiguration;
+import ch.welld.schindler.fixture.droolsknowledge.components.controldevices.PositionSetup;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@ApplicationScoped
+public class ControlDeviceConfigurationBuilder extends AbstractConfigurationBuilder {
+
+    @Override
+    public boolean canParseConfiguration(Map<String, Object> config) {
+        return "Control Device".equalsIgnoreCase((String) config.get("elevator"));
+    }
+
+    @Override
+    public List<ComponentConfiguration> getConfigurationsImpl(Map<String, Object> config) {
+        ControlDeviceConfiguration cdc = new ControlDeviceConfiguration();
+
+        cdc.setType(((String) config.get("controlDevices")).toUpperCase());
+        cdc.setVersion(Optional
+            .ofNullable(config.get("version"))
+            .map(text -> ((String) text).toUpperCase())
+            .orElse(null));
+
+        cdc.setPositions(getPositions(config));
+
+        return Collections.singletonList(
+                new ComponentConfiguration(
+                    cdc,
+                    1
+                )
+        );
+    }
+
+    private Map<String, List<PositionSetup>> getPositions(Map<String, Object> config) {
+        Object onCommissionBoxObj = config.get("onCommissionBox");
+        if (onCommissionBoxObj == null) {
+            return null;
+        }
+        Map<String, Map<String, String>> onCommissionBox = (Map<String, Map<String, String>>) onCommissionBoxObj;
+
+        Map<String, List<PositionSetup>> positionsSetupsByType = new HashMap<>();
+
+        onCommissionBox.forEach((key, value) -> {
+            String type = value.get("selection").toUpperCase();
+            if (!positionsSetupsByType.containsKey(type)) {
+                positionsSetupsByType.put(type, new ArrayList<>());
+            }
+            List<PositionSetup> typePositionSetups = positionsSetupsByType.get(type);
+            typePositionSetups.add(new PositionSetup(key, value.get("text")));
+        });
+
+        return positionsSetupsByType;
+    }
+}
